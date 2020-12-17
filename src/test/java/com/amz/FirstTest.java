0af8ec0fa19e9ccedd2d9e01151734d7e25bfbaa
OleksiiHavrylov <oleksii.havrylov.ua@remotal.io>
2020-12-17 23:59:05 +0200

    Added description

--- src/test/java/com/amz/FirstTest.java
+++ src/test/java/com/amz/FirstTest.java
@@ -14,12 +14,14 @@ public class FirstTest {
         ChromeDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "/driver/chromedriver");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//1. Go to https://www.amazon.com
         driver.get("https://www.amazon.com");
-        driver.manage().window().maximize() ;
+        driver.manage().window().maximize();
         WebElement search_box = driver.findElement(By.id("twotabsearchtextbox"));
+//2. Search for "hats for men"
         search_box.sendKeys("hats for men");
         search_box.sendKeys(Keys.ENTER);
-
+//3. Add first hat to Cart with quantity 2
         WebElement price = driver.findElement(By.className("a-price-whole"));
         price.click();
         WebElement setQnt = driver.findElement(By.id("a-autoid-9"));
@@ -28,7 +30,7 @@ public class FirstTest {
 
         WebElement AddToCart = driver.findElement(By.name("submit.addToCart"));
         AddToCart.click();
-
+//4.Open cart and assert total price and quantity are correct
         driver.quit();
 
 
